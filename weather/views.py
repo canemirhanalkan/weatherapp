01a252f6e002995b316848e82bbc6e63f185bfbf
Can Alkan <alkncann@gmail.com>
2024-05-21 20:37:18 +0300

    complated project

--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
+import requests
+import datetime
+import locale
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'weather/index.html')
+
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'İstanbul'
+
+    key = '12f9563b5d56a94d23d5c83b004ed158'
+    URL = 'https://api.openweathermap.org/data/2.5/weather'
+    PARAMS = {'q':city, 'appid':key, 'units':'metric', 'lang':'tr'}
+    r = requests.get(url = URL, params=PARAMS)
+    res = r.json()
+    
+    description = res['weather'][0]['description'].upper()
+    icon = res['weather'][0]['icon']
+    temp = res['main']['temp']
+
+    locale.setlocale(locale.LC_ALL,"")
+    day = datetime.date.today().strftime("%A")
+
+    return render(request, 'weather/index.html', {
+        'description':description,
+        'icon':icon,
+        'temp':temp,
+        'day':day,
+        'city':city,
+    })
